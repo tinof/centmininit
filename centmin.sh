e96de28ae37a1796b550c18d6277f6f54b7a9471
Tino Fotiou <tino.fotiou@gmail.com>
2019-08-10 14:35:32 +0300

    Update centmin.sh

--- centmin.sh
+++ centmin.sh
@@ -3,39 +3,7 @@
 # https://centminmod.com/upgrade.html#persistent
 mkdir -p /etc/centminmod
 touch /etc/centminmod/custom_config.inc
-
-# dynamically tune nginx ssl_session_cache in /usr/local/nginx/conf/ssl_include.conf based on system detected memory
-# https://community.centminmod.com/posts/76615/
-echo "NGINX_SSLCACHE_ALLOWOVERRIDE='y'" >> /etc/centminmod/custom_config.inc
-
-# SET_DEFAULT_MYSQLCHARSET='utf8mb4' to override MariaDB MySQL
-# default characterset and collation from default utf8 to utf8mb4
-# https://community.centminmod.com/threads/17949/
-echo "SET_DEFAULT_MYSQLCHARSET='utf8mb4'" >> /etc/centminmod/custom_config.inc
-
-# enable nginx backlog override https://community.centminmod.com/threads/17620/
-echo "AUTOHARDTUNE_NGINXBACKLOG='y'" >> /etc/centminmod/custom_config.inc
-
-# enable zstd compressed logrotation for nginx & php-fpm https://community.centminmod.com/threads/16374/
-echo "ZSTD_LOGROTATE_NGINX='y'" >> /etc/centminmod/custom_config.inc
-echo "ZSTD_LOGROTATE_PHPFPM='y'" >> /etc/centminmod/custom_config.inc
-
-# enable nginx zero downtime on the fly nginx binary upgrades https://community.centminmod.com/threads/8000/
-#echo "NGINX_ZERODT='y'" >> /etc/centminmod/custom_config.inc
-
-# enable brotli compression https://community.centminmod.com/threads/10688/
-echo "NGINX_LIBBROTLI='y'" >> /etc/centminmod/custom_config.inc
-echo "NGXDYNAMIC_BROTLI='y'" >> /etc/centminmod/custom_config.inc
-
-# boost PHP 7 performance by enabling Profile Guided Optimisation flag
-# https://centminmod.com/perf/
-echo "PHP_PGO='y'" >> /etc/centminmod/custom_config.inc
-
-# php compression extensions https://community.centminmod.com/posts/70777/
-echo "PHP_BROTLI='y'" >> /etc/centminmod/custom_config.inc
-echo "PHP_LZFOUR='y'" >> /etc/centminmod/custom_config.inc
-echo "PHP_LZF='y'" >> /etc/centminmod/custom_config.inc
-echo "PHP_ZSTD='y'" >> /etc/centminmod/custom_config.inc
+yum -y install nano
 
 # install centmin mod latest beta with php-fpm 7.2 default
 # https://community.centminmod.com/threads/centmin-mod-09-beta-branch-testing.4128/
