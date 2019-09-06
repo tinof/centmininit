a765ce5e3c75390297165347e2d536726da6fba8
Tino Fotiou <tino.fotiou@gmail.com>
2019-09-06 15:49:00 +0300

    Update mysqladmin_shell.sh

--- mysqladmin_shell.sh
+++ mysqladmin_shell.sh
@@ -2,3 +2,4 @@ mkdir -p /root/tools
 cd /root/tools/
 wget -O mysqladmin_shell.sh https://github.com/centminmod/centminmod/raw/123.09beta01/addons/mysqladmin_shell.sh
 chmod +x mysqladmin_shell.sh
+/root/tools/mysqladmin_shell.sh setuserdb
