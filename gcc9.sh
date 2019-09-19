44c75dae11224d28a048f653469399b029ece121
Tino Fotiou <tino.fotiou@gmail.com>
2019-09-19 18:48:11 +0300

    Update gcc9.sh

--- gcc9.sh
+++ gcc9.sh
@@ -5,5 +5,7 @@ sudo yum -y install bzip2 wget gcc gcc-c++ gmp-devel mpfr-devel libmpc-devel mak
 gcc --version
 wget http://gnu.mirror.constant.com/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz
 tar zxf gcc-$GCC_VERSION.tar.gz
+cd gcc-$GCC_VERSION
+./contrib/download_prerequisites
 mkdir gcc-build
 cd gcc-build
