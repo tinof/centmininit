38bf7c8abcf41e21ef867f04815a5a5304389631
Tino Fotiou <tino.fotiou@gmail.com>
2019-09-23 14:24:42 +0300

    Update gcc9builder.sh

--- gcc9builder.sh
+++ gcc9builder.sh
@@ -1,5 +1,7 @@
 yum -y group install "Development Tools"
 yum -y install redhat-lsb
+yum -y install nano
+wget https://raw.githubusercontent.com/tinof/centmininit/master/gcc-build-vars.sh -O ./gcc-build-vars.sh
 mkdir gccb
 cd gccb
 git clone https://github.com/BobSteagall/gcc-builder.git
