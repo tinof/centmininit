3de88bfdaf0cf7351a14ba94770267c3a2eb2310
Tino Fotiou <tino.fotiou@gmail.com>
2019-09-25 15:17:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,15 +45,15 @@ MPC_FILE='mpc-1.1.0.tar.gz'
 MPFR_FILE='mpfr-4.0.2.tar.bz2'
 
 CLANG_FOUR='n'
-OPT_LEVEL=-O3
+OPT_LEVEL=-O2
 CCACHE='y'
 CENTMINLOGDIR='/root/centminlogs'
 GCC_SNAPSHOTSEVEN='http://www.netgull.com/gcc/snapshots/LATEST-7/'
 #GCC_SNAPSHOTSEVEN='http://www.netgull.com/gcc/releases/gcc-7.3.0/'
 GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/snapshots/LATEST-8/'
 #GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/releases/gcc-8.2.0/'
-#GCC_SNAPSHOTNINE='http://www.netgull.com/gcc/snapshots/LATEST-9/'
-GCC_SNAPSHOTNINE='https://gnu.freemirror.org/gnu/gcc/gcc-9.2.0/'
+GCC_SNAPSHOTNINE='http://www.netgull.com/gcc/snapshots/LATEST-9/'
+#GCC_SNAPSHOTNINE='https://gnu.freemirror.org/gnu/gcc/gcc-9.2.0/'
 #GCC_SNAPSHOTNINE='http://www.netgull.com/gcc/snapshots/10-20190922/'
 GCC_COMPILEOPTS='--disable-multilib --enable-bootstrap --enable-plugin --with-gcc-major-version-only --enable-shared --disable-nls --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-install-libiberty --disable-libunwind-exceptions --enable-gnu-unique-object --enable-linker-build-id --with-linker-hash-style=gnu --enable-languages=c,c++ --enable-initfini-array --disable-libgcj --enable-gnu-indirect-function --with-tune=generic --build=x86_64-redhat-linux --enable-lto --enable-gold
 Thread model: posix'
