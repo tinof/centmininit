7b6ac48bc38e37a9c2103df5557a4a067f527ad5
Tino Fotiou <tino.fotiou@gmail.com>
2019-09-25 13:28:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,21 +20,21 @@ RPMSAVE_PATH="$DIR_TMP"
 GCC_YUMINSTALL='n'
 
 # SVN GCC 7 or 8 or 9
-GCCSVN_VER='8'
+GCCSVN_VER='9'
 GCC_SVN='y'
-GCC_VER='7.2.1'
+GCC_VER='9.2.0'
 GCC_PREFIX="/opt/gcc-${GCC_VER}"
 # download from ftp://gcc.gnu.org/pub/gcc/infrastructure/
 # or via wget code for more reliability as
 # ./contrib/download_prerequisites script sees to fail to
 # download some required packages each time it runs
-GCC_DOWNLOADPREREQ='n'
+GCC_DOWNLOADPREREQ='y'
 GCC_LTO='y'
 GCC_GOLD='y'
 # Profile Guided Optimiized GCC build
 # using profiledbootstrap
 # https://gcc.gnu.org/install/build.html
-GCC_PGO='n'
+GCC_PGO='y'
 BOOTCFLAGS='y'
 BINUTILS_VER='2.32'
 
@@ -45,15 +45,15 @@ MPC_FILE='mpc-1.1.0.tar.gz'
 MPFR_FILE='mpfr-4.0.2.tar.bz2'
 
 CLANG_FOUR='n'
-OPT_LEVEL=-O2
+OPT_LEVEL=-O3
 CCACHE='y'
 CENTMINLOGDIR='/root/centminlogs'
 GCC_SNAPSHOTSEVEN='http://www.netgull.com/gcc/snapshots/LATEST-7/'
 #GCC_SNAPSHOTSEVEN='http://www.netgull.com/gcc/releases/gcc-7.3.0/'
 GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/snapshots/LATEST-8/'
 #GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/releases/gcc-8.2.0/'
-#GCC_SNAPSHOTNINE='http://www.netgull.com/gcc/snapshots/LATEST-9/'
-GCC_SNAPSHOTNINE='https://gnu.freemirror.org/gnu/gcc/gcc-9.2.0/'
+GCC_SNAPSHOTNINE='http://www.netgull.com/gcc/snapshots/LATEST-9/'
+#GCC_SNAPSHOTNINE='https://gnu.freemirror.org/gnu/gcc/gcc-9.2.0/'
 #GCC_SNAPSHOTNINE='http://www.netgull.com/gcc/snapshots/10-20190922/'
 GCC_COMPILEOPTS='--enable-bootstrap --enable-plugin --with-gcc-major-version-only --enable-shared --disable-nls --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-install-libiberty --disable-libunwind-exceptions --enable-gnu-unique-object --enable-linker-build-id --with-linker-hash-style=gnu --enable-languages=c,c++ --enable-initfini-array --disable-libgcj --enable-gnu-indirect-function --with-tune=generic --build=x86_64-redhat-linux'
 SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
