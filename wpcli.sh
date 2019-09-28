2ba5732f10bbed406d4864b109bd6d93faf7ff8c
Tino Fotiou <tino.fotiou@gmail.com>
2019-09-28 15:24:38 +0300

    Create wpcli.sh

---
+++ wpcli.sh
@@ -0,0 +1,2 @@
+cd /home/nginx/domains/yourdomain.com/public/wp
+  wp core install --url='http://yourdomain.com/wp' --title='your wordpress site title' --admin_email='youremail@yourdomain.com' --admin_password='yourwordpressadminpass' --admin_name=adminuser
