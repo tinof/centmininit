5e71e4d57284c7509e408e6c79fee9461ab8943f
Tino Fotiou <tino.fotiou@gmail.com>
2019-10-01 22:48:33 +0300

    Update centmin.sh

--- centmin.sh
+++ centmin.sh
@@ -26,8 +26,6 @@ egrep 'CUSTOM1_LOG|CUSTOM2_LOG|CUSTOM3_LOG|CUSTOM4_LOG' /etc/csf/csf.conf
 wget -O /usr/local/csf/bin/regex.custom.pm https://gist.github.com/centminmod/f5551b92b8aba768c3b4db84c57e756d/raw/regex.custom.pm
 csf -ra
 
-mkdir -p /root/tools
-
 yum -y install wget; wget https://raw.githubusercontent.com/tinof/centmininit/master/install.sh; chmod +x install.sh; ./install.sh install9
 
 echo "Done!"
