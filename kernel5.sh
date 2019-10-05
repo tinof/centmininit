352da7517c2c2cd9bd2f5caa49e2ed76256912b4
Tino Fotiou <tino.fotiou@gmail.com>
2019-10-05 16:59:55 +0300

    Create kernel5.sh

---
+++ kernel5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+yum -y --enablerepo=elrepo-kernel install kernel-ml
+grub2-set-default 0
+grub2-mkconfig -o /boot/grub2/grub.cfg
+reboot
