f92506e7bc31b2003115a2a23320323036a72a74
Tino Fotiou <tino.fotiou@gmail.com>
2019-11-03 16:30:44 +0200

    Update kernel5.sh

--- kernel5.sh
+++ kernel5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
 yum -y --enablerepo=elrepo-kernel install kernel-ml
 grub2-set-default 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
