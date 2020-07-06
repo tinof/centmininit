85d76c8141a369a27fecead0ad144e5da629467a
Tino Fotiou <tino.fotiou@gmail.com>
2020-07-06 17:23:51 +0300

    Update cmvarnish.sh

--- cmvarnish.sh
+++ cmvarnish.sh
@@ -17,4 +17,26 @@ chmod a+x /usr/bin/hitch-deploy-hook
 usermod -a -G varnish hitch
 service hitch restart
 
+#Since we’re going to store socket files in a dedicated directory, /var/run/varnish, we will “tell” SELinux what we’re going to use the directory for:
+semanage fcontext -a -t varnishd_var_run_t "/var/run/varnish(/.*)?"
+
+#And we also make sure that our sockets directory is created after reboot:
+cat << _EOF_ >> /etc/tmpfiles.d/varnish.conf
+  d /run/varnish 755 varnish varnish
+_EOF_
+
+#And we also create it right away via:
+systemd-tmpfiles --create varnish.conf
+
+#Make sure to setup correct SELinux label to our directory:
+semanage fcontext -a -t httpd_var_run_t "/var/run/nginx(/.*)?"
+
+#And ensure that the directory is created at boot time:
+cat << _EOF_ >> /etc/tmpfiles.d/nginx.conf
+  d /run/nginx 750 nginx varnish
+_EOF_
+
+#And we also create it right away via:
+systemd-tmpfiles --create nginx.conf
+
 
