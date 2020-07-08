8c06ca2cba2419658f128482647775d6084a55d8
Tino Fotiou <tino.fotiou@gmail.com>
2020-07-08 12:44:17 +0300

    Update kernel5.sh

--- kernel5.sh
+++ kernel5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-yum install https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm
+yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 yum -y --enablerepo=elrepo-kernel install kernel-ml
 grub2-set-default 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
