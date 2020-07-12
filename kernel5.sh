96b602b6f33af6af0cf437227f45ec0e47596c8e
Tino Fotiou <tino.fotiou@gmail.com>
2020-07-12 20:42:21 +0300

    Update kernel5.sh

--- kernel5.sh
+++ kernel5.sh
@@ -4,4 +4,5 @@ yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 yum -y --enablerepo=elrepo-kernel install kernel-ml
 grub2-set-default 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
+package-cleanup --oldkernels --count=1
 reboot
