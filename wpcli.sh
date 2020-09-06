dd43500c765e7fb829898d21aaec3d3d4d20c543
Tino Fotiou <tino.fotiou@gmail.com>
2020-09-06 11:26:15 +0300

    Delete wpcli.sh

--- wpcli.sh
+++
@@ -1,2 +0,0 @@
-cd /home/nginx/domains/yourdomain.com/public/wp
-  wp core install --url='http://yourdomain.com/wp' --title='your wordpress site title' --admin_email='youremail@yourdomain.com' --admin_password='yourwordpressadminpass' --admin_name=adminuser
