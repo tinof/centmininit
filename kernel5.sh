408862bb4726a46fa0294103dd6a1d312f0086a6
Tino Fotiou <tino.fotiou@gmail.com>
2020-09-06 11:33:20 +0300

    Update kernel5.sh

--- kernel5.sh
+++ kernel5.sh
@@ -4,5 +4,7 @@ yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 yum -y --enablerepo=elrepo-kernel install kernel-ml
 grub2-set-default 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
-package-cleanup --oldkernels --count=1
+#wget https://raw.githubusercontent.com/tinof/centmininit/master/wpsetup.inc  -O /usr/local/src/centminmod/inc/wpsetup.inc 
+#chmod +x /etc/rc.d/rc.local
+#echo "WPCLI_CE_QUERYSTRING_INCLUDED='y'" >> /etc/centminmod/custom_config.inc
 reboot
