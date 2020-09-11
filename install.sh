26862d9e3b9ac93f6db03f68b0a18ac9c3bbbe5a
Tino Fotiou <tino.fotiou@gmail.com>
2020-09-11 20:43:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,14 +22,14 @@ GCC_YUMINSTALL='y'
 # SVN GCC 7 or 8 or 9
 GCCSVN_VER='9'
 GCC_SVN='y'
-GCC_VER='9.3.1'
+GCC_VER='9.2.1'
 GCC_PREFIX="/opt/gcc-${GCC_VER}"
 # download from ftp://gcc.gnu.org/pub/gcc/infrastructure/
 # or via wget code for more reliability as
 # ./contrib/download_prerequisites script sees to fail to
 # download some required packages each time it runs
 GCC_DOWNLOADPREREQ='y'
-GCC_LTO='n'
+GCC_LTO='y'
 GCC_GOLD='y'
 # Profile Guided Optimiized GCC build
 # using profiledbootstrap
@@ -39,9 +39,9 @@ BOOTCFLAGS='y'
 BINUTILS_VER='2.33.1'
 
 # GCC Downloads
-GMP_FILE='gmp-6.1.0.tar.bz2'
+GMP_FILE='gmp-6.1.2.tar.bz2'
 ISL_FILE='isl-0.18.tar.bz2'
-MPC_FILE='mpc-1.0.3.tar.gz'
+MPC_FILE='mpc-1.1.0.tar.gz'
 MPFR_FILE='mpfr-3.1.4.tar.bz2'
 
 CLANG_FOUR='n'
@@ -52,10 +52,10 @@ GCC_SNAPSHOTSEVEN='http://www.netgull.com/gcc/snapshots/LATEST-7/'
 #GCC_SNAPSHOTSEVEN='http://www.netgull.com/gcc/releases/gcc-7.3.0/'
 GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/snapshots/LATEST-8/'
 #GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/releases/gcc-8.2.0/'
-#GCC_SNAPSHOTNINE='http://mirror.koddos.net/gcc/snapshots/LATEST-9/'
-GCC_SNAPSHOTNINE='https://bigsearcher.com/mirrors/gcc/snapshots/LATEST-10/'
+GCC_SNAPSHOTNINE='http://mirror.koddos.net/gcc/snapshots/LATEST-9/'
+#GCC_SNAPSHOTNINE='https://gnu.freemirror.org/gnu/gcc/gcc-9.2.0/'
 #GCC_SNAPSHOTNINE='http://mirrors.concertpass.com/gcc/releases/gcc-9.2.0/'
-GCC_COMPILEOPTS='--disable-multilib --enable-bootstrap --enable-plugin --with-gcc-major-version-only --enable-shared --disable-nls --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-install-libiberty --disable-libunwind-exceptions --enable-gnu-unique-object --enable-linker-build-id --with-linker-hash-style=gnu --enable-languages=c,c++ --enable-initfini-array --disable-libgcj --enable-gnu-indirect-function --with-tune=znver2 --build=x86_64-redhat-linux --enable-lto --enable-gold'
+GCC_COMPILEOPTS='--enable-bootstrap --enable-plugin --with-gcc-major-version-only --enable-shared --disable-nls --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-install-libiberty --disable-libunwind-exceptions --enable-gnu-unique-object --enable-linker-build-id --with-linker-hash-style=gnu --enable-languages=c,c++ --enable-initfini-array --disable-libgcj --enable-gnu-indirect-function --with-tune=znver2 --build=x86_64-redhat-linux --enable-lto --enable-gold'
 SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 ################################################
 # Setup Colours
