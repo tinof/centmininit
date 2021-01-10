d9e077791488023c26de5cca69934c904c2bc0ed
Tino Fotiou <tino.fotiou@gmail.com>
2021-01-10 19:49:54 +0200

    Update centmin.sh

--- centmin.sh
+++ centmin.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+yum -y install nano;yum -y install wget yum-utils; yum -y upgrade
 # create initial persistent config file to override centmin.sh defaults
 # https://centminmod.com/upgrade.html#persistent
 mkdir -p /etc/centminmod
-yum -y install nano
 wget https://raw.githubusercontent.com/tinof/centmininit/master/custom_config.inc -O /etc/centminmod/custom_config.inc
-
 
 # install centmin mod latest beta with php-fpm 7.4 default
 yum -y update; curl -O https://raw.githubusercontent.com/centminmod/centminmod/123.09beta01/installer74.sh && chmod 0700 installer74.sh && bash installer74.sh
 
+# pre-create Nginx HTTPS site’s dhparam file before hand to speed up subsequent Nginx vhost creation routines. On slow systems, this command will take a few minutes to complete.
+openssl dhparam -out /usr/local/nginx/conf/ssl/dhparam.pem 2048
+
+# setup extended CSF Firewall blocklists https://community.centminmod.com/posts/50060/
+/usr/local/src/centminmod/tools/csf-advancetweaks.sh
 
 # enable CSF Firewall native fail2ban like support
 # https://community.centminmod.com/posts/62343/
@@ -23,10 +27,20 @@ egrep 'CUSTOM1_LOG|CUSTOM2_LOG|CUSTOM3_LOG|CUSTOM4_LOG' /etc/csf/csf.conf
 wget -O /usr/local/csf/bin/regex.custom.pm https://gist.github.com/centminmod/f5551b92b8aba768c3b4db84c57e756d/raw/regex.custom.pm
 csf -ra
 
+# cloudflare cronjob
+# https://community.centminmod.com/threads/6241/
+crontab -l > cronjoblist
+sed -i '/csfcf.sh/d' cronjoblist
+echo "23 */12 * * * /usr/local/src/centminmod/tools/csfcf.sh auto >/dev/null 2>&1" >> cronjoblist
+crontab cronjoblist
+
 
 # MALDET 
 wget https://raw.githubusercontent.com/tinof/centmininit/master/maldet.sh -O /usr/local/src/centminmod/addons/maldet.sh
 /usr/local/src/centminmod/addons/maldet.sh
 
+cd /usr/local/src/centminmod/addons/; ./customcurl.sh
+yum update --enablerepo=city-fan.org --disableplugin=priorities
+package-cleanup --oldkernels --count=1
 
 echo "Done!"
