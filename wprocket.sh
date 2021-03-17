97ada33d1562ac731210fdaf77f131d056a90199
Tino Fotiou <tino.fotiou@gmail.com>
2021-03-17 22:34:54 +0200

    Update wprocket.sh

--- wprocket.sh
+++ wprocket.sh
@@ -5,9 +5,9 @@ printf "\n\n${grn}To install WP Rocket Nginx support for centminmod.com hosted d
 read domain
 
 sed -i 's/php-wpsc/php/g' /usr/local/nginx/conf/conf.d/$domain.ssl.conf
-sed -i '78i s/^/#/' /usr/local/nginx/conf/conf.d/$domain.ssl.conf
-sed -i -e "79i try_files $uri $uri/ /index.php?$args;" /usr/local/nginx/conf/conf.d/$domain.ssl.conf
-sed -i -e "137i include /usr/local/nginx/rocket-nginx/default.conf;"/usr/local/nginx/conf/conf.d/$domain.ssl.conf
+##sed -i '78i s/^/#/' /usr/local/nginx/conf/conf.d/$domain.ssl.conf <- NEEDs FIXING
+##sed -i -e "79i try_files $uri $uri/ /index.php?$args;" /usr/local/nginx/conf/conf.d/$domain.ssl.conf
+##sed -i -e "137i include /usr/local/nginx/rocket-nginx/default.conf;"/usr/local/nginx/conf/conf.d/$domain.ssl.conf
 
 cd /usr/local/nginx
 git clone https://github.com/SatelliteWP/rocket-nginx.git
