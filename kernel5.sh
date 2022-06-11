5150f28e23ddb48e2d997f130905ee9843e26fe6
Tino Fotiou <tino.fotiou@gmail.com>
2022-06-11 19:37:24 +0300

    Update kernel5.sh

--- kernel5.sh
+++ kernel5.sh
@@ -1,6 +1,7 @@
 yum -y upgrade
 yum -y install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+echo "include_only=.nl,.fi" >> /etc/yum/pluginconf.d/fastestmirror.conf
 yum -y --enablerepo=elrepo-kernel install kernel-ml
 echo "GRUB_DEFAULT=0" >> /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
