6fe4ac17de39e0a632369a6f4e248df79960cbe3
Tino Fotiou <tino.fotiou@gmail.com>
2022-06-11 19:09:11 +0300

    Update kernel5.sh

--- kernel5.sh
+++ kernel5.sh
@@ -2,5 +2,5 @@ yum -y upgrade
 yum -y install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 yum -y --enablerepo=elrepo-kernel install kernel-ml
-
-#grub2-mkconfig -o /boot/grub2/grub.cfg
+echo "GRUB_DEFAULT=0" >> /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
