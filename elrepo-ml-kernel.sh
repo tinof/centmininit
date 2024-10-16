60dd0f1dbf09a442115da1ef19cab856bb627499
Tino Fotiou <tino.fotiou@gmail.com>
2024-10-16 17:21:42 +0300

    Update elrepo-ml-kernel.sh

--- elrepo-ml-kernel.sh
+++ elrepo-ml-kernel.sh
@@ -1,6 +1,6 @@
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 dnf install -y https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm
 dnf update
-echo "include_only=.nl,.fi" >> /etc/yum/pluginconf.d/fastestmirror.conf
-dnf --enablerepo=elrepo-kernel install kernel-ml
+echo "include_only=.nl,.fi" | sudo tee -a /etc/yum/pluginconf.d/fastestmirror.conf
+dnf --enablerepo=elrepo-kernel install kernel-ml -y
 reboot
