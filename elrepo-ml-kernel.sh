dad9fe6b9c6ef3fe03cb612d377333e0d5184ebd
Tino Fotiou <tino.fotiou@gmail.com>
2024-10-16 23:20:37 +0300

    Update elrepo-ml-kernel.sh

--- elrepo-ml-kernel.sh
+++ elrepo-ml-kernel.sh
@@ -8,7 +8,7 @@ sudo dnf install -y https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rp
 sudo dnf update -y
 
 # (Optional) Configure the fastestmirror plugin to use specific country mirrors
-echo "include_only=.nl,.fi" | sudo tee -a /etc/yum/pluginconf.d/fastestmirror.conf
+#echo "include_only=.nl,.fi" | sudo tee -a /etc/yum/pluginconf.d/fastestmirror.conf
 
 # Install the mainline kernel from ELRepo
 sudo dnf --enablerepo=elrepo-kernel install kernel-ml -y
